0100ad3d377b12dc4e91208de636a007321e5268
jasminetagwercher-alt <jasmine.tagwercher@gmail.com>
2026-09-10 23:33:57 +0200

    Configure shorter mission-based learning flow

--- js/config.js
+++ js/config.js
@@ -1,11 +1,9 @@
 export const CONFIG={
   appName:'Bruch-Check',
-  storageKey:'bruch-check-session-v1',
+  storageKey:'bruch-check-session-v3',
   remoteStorageUrl:'',
-  diagnosticLength:14,
-  trainingLength:12,
-  mixLength:10,
-  finalLength:10,
-  breakEvery:5,
-  recentTaskMemory:16
+  diagnosticLength:10,
+  finalLength:8,
+  quickBreakAfter:5,
+  recentTaskMemory:20
 };
