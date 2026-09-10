a3239874efe4014fa3ea77f70d36bcc021b2abf5
jasminetagwercher-alt <jasmine.tagwercher@gmail.com>
2026-09-10 23:07:54 +0200

    Add Austrian maths terminology registry

---
+++ js/didactics/terminology-at.js
@@ -0,0 +1,20 @@
+export const AT_TERMS=Object.freeze({
+  numerator:'Zähler',
+  denominator:'Nenner',
+  fractionBar:'Bruchstrich',
+  properFraction:'echter Bruch',
+  improperFraction:'unechter Bruch',
+  mixedNumber:'gemischte Zahl',
+  equivalentFractions:'gleichwertige Brüche',
+  commonDenominator:'gemeinsamer Nenner',
+  makeLikeDenominators:'gleichnamig machen',
+  reciprocal:'Kehrwert',
+  reduce:'kürzen',
+  expand:'erweitern',
+  dividend:'Dividend',
+  divisor:'Divisor',
+  product:'Produkt',
+  quotient:'Quotient'
+});
+
+export function term(key){return AT_TERMS[key]||key}
