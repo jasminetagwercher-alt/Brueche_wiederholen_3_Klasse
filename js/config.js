6057683b71ef604e8da31e2b8e28248499b32c9b
jasminetagwercher-alt <jasmine.tagwercher@gmail.com>
2026-09-11 07:18:39 +0200

    Version storage for task sheet flow

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 export const CONFIG={
   appName:'Bruch-Check',
-  storageKey:'bruch-check-session-v3',
+  storageKey:'bruch-check-session-v4',
   remoteStorageUrl:'',
   diagnosticLength:10,
   finalLength:8,
   quickBreakAfter:5,
-  recentTaskMemory:20
+  recentTaskMemory:24
 };
